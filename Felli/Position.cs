8ce8d49d5ed45dd57864c3339728ca1f58de1679
Freeze-2 <avdigitaldesigns@gmail.com>
2020-05-01 16:31:59 +0100

    Add Board.cs and Position.cs

---
+++ Felli/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Felli
+{
+    public struct Position
+    {
+        private byte X { get; }
+        private byte Y { get; }
+
+        public Position(byte x = 0, byte y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
