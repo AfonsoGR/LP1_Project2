26f905702cf8ee1491bfbd9ed895924b2da687ec
AfonsoGR <afonso_rosa@hotmail.com>
2020-05-04 18:20:48 +0100

    Add piece capture

--- Felli/Piece.cs
+++ Felli/Piece.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Felli
 {
@@ -26,13 +27,16 @@ namespace Felli
             board[piecePos.X, piecePos.Y] = visuals;
         }
 
-        private string MovementCoordinates(int x, int y)
+        private string MovementCoordinates(Player player,int x, int y)
         {
             if ((board[piecePos.X + x, piecePos.Y + y] != visuals &&
                 board[piecePos.X + x, piecePos.Y + y] != ColorChoice.None) &&
                 (board[piecePos.X + (x + x), piecePos.Y + (y + y)] 
                 == ColorChoice.None))
-            { 
+            {
+                Position position = 
+                    new Position(piecePos.X + x, piecePos.Y + y);
+                CapturePiece(player, position); 
                 piecePos += ((x + x), (y + y));
                 return null;
             }
@@ -46,6 +50,23 @@ namespace Felli
                 return "Your piece can't move that way.";
             }
         }
+
+        public void CapturePiece(Player player, Position position)
+        {
+            //Piece[] newPieces = new Piece[player.playerPieces.Length - 1];
+            List<Piece> capturedPieces = new List<Piece>(); 
+            
+            for (int i = 0; i < player.playerPieces.Length; i++)
+            {
+                if (player.playerPieces[i].piecePos != position)
+                {
+                    capturedPieces.Add(player.playerPieces[i]);
+                } 
+            }
+
+            player.playerPieces = capturedPieces.ToArray();
+        }
+
         public string PieceMovement(Player opositePlayer)
         {
             int moveChoice;
@@ -62,49 +83,49 @@ namespace Felli
             if (moveChoice == 1)
             {
                 return piecePos.X < 5 && piecePos.Y > 0 ? 
-                    MovementCoordinates(1, -1) 
+                    MovementCoordinates(opositePlayer, 1, -1) 
                     : "Your piece can't move that way.";
             }
             else if (moveChoice == 2)
             {
                 return piecePos.X < 5 ? 
-                    MovementCoordinates(1, 0) 
+                    MovementCoordinates(opositePlayer, 1, 0) 
                     : "Your piece can't move that way.";
             }
             else if (moveChoice == 3)
             {
                 return piecePos.X < 5 && piecePos.Y < 5 ? 
-                    MovementCoordinates(1, 1) 
+                    MovementCoordinates(opositePlayer, 1, 1) 
                     : "Your piece can't move that way.";
             }
             else if (moveChoice == 4)
             {
                 return piecePos.Y > 0 ? 
-                    MovementCoordinates(0, -1) 
+                    MovementCoordinates(opositePlayer, 0, -1) 
                     : "Your piece can't move that way.";
             }
             else if (moveChoice == 6)
             {
                 return piecePos.Y < 5 ? 
-                    MovementCoordinates(0, 1) 
+                    MovementCoordinates(opositePlayer, 0, 1) 
                     : "Your piece can't move that way.";
             }
             else if (moveChoice == 7)
             {
                 return piecePos.X > 0 && piecePos.Y > 0 ? 
-                    MovementCoordinates(-1, -1) 
+                    MovementCoordinates(opositePlayer, -1, -1) 
                     : "Your piece can't move that way.";
             }
             else if (moveChoice == 8)
             {
                 return piecePos.X > 0 ? 
-                    MovementCoordinates(-1, 0) 
+                    MovementCoordinates(opositePlayer, -1, 0) 
                     : "Your piece can't move that way.";
             }
             else if (moveChoice == 9)
             {
                 return piecePos.X > 0 && piecePos.Y < 5 ? 
-                    MovementCoordinates(-1, 1) 
+                    MovementCoordinates(opositePlayer, -1, 1) 
                     : "Your piece can't move that way.";
             }
             else
